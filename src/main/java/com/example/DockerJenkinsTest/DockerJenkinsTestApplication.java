79b1a5efc1016c81d4586de5071928a92c051051
Berlinda Babe <Berlinda@Beibeis-MacBook-Pro.local>
2023-03-22 17:04:45 -0400

    first commit

---
+++ src/main/java/com/example/DockerJenkinsTest/DockerJenkinsTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.DockerJenkinsTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerJenkinsTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerJenkinsTestApplication.class, args);
+	}
+
+}
